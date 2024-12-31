1022d826871cf0dee5bc1ce58bbd654cf52cc4c1
Veer Hegde <veer165@gmail.com>
2024-12-31 11:38:49 +0530

    added 9 questions

--- psy_fe/mindq_ui/lib/screens/start_onboard.dart
+++ psy_fe/mindq_ui/lib/screens/start_onboard.dart
@@ -29,7 +29,7 @@ class _StartOnboardingScreenState extends State<StartOnboardingScreen> {
                 ),
 
                 Positioned(
-                  bottom: 30,
+                  bottom: 50,
                   child: Container(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 30, vertical: 15),
@@ -47,17 +47,83 @@ class _StartOnboardingScreenState extends State<StartOnboardingScreen> {
                     child:
                     RichText(
                       text: TextSpan(
-                        text: 'Hi There! my name is ',
-                        style: GoogleFonts.roboto(fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
-                        children: <TextSpan>[
-                          TextSpan(text: 'Phoebe', style: GoogleFonts.getFont('Poppins', textStyle: const TextStyle(
-                            color: Colors.deepPurple, decoration: TextDecoration.underline
+                        style: TextStyle(fontSize: 18.0, color: Colors.black),
+                        children: [
+                          TextSpan(
+                            text: 'Hi there! ',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        TextSpan(
+                          text: 'my name is ',
+                        ),
+
+                          TextSpan(text: 'Phoebe\n', style: GoogleFonts.getFont('Poppins', textStyle: const TextStyle(
+                              color: Color(0xffb74093), fontWeight: FontWeight.bold, fontStyle: FontStyle.italic
                           ))),
-                        const TextSpan(
-                        text: '. \nI’ll be your guide, buddy, partner-in-crime!\n Sooo, since it’s our first hangout, let’s get\nto know your vibe—what makes you tick,\ngroove, or go ’meh!’ Sound fun?\n Let’s do this!',)
+                          TextSpan(
+                            text: 'I’m your',
+                          ),
+                          TextSpan(
+                            text: ' guide, ',
+                            style: TextStyle(fontStyle: FontStyle.italic),
+                          ),
+                          TextSpan(
+                            text: 'buddy, ',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: 'partner-in-crime',
+                            style: TextStyle(fontStyle: FontStyle.italic),
+                          ),
+                          TextSpan(
+                            text: '!\n Sooo, since it’s our ',
+                          ),
+                          TextSpan(
+                            text: 'first hangout',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: ', let’s\nget to know your ',
+                          ),
+                          TextSpan(
+                            text: 'vibe',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: '—what makes you\n',
+                          ),
+                          TextSpan(
+                            text: 'tick',
+                            style: TextStyle(fontStyle: FontStyle.italic),
+                          ),
+                          TextSpan(
+                            text: ', ',
+                          ),
+                          TextSpan(
+                            text: 'groove',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: ', or go ',
+                          ),
+                          TextSpan(
+                            text: '\'meh!\'',
+                            style: TextStyle(fontStyle: FontStyle.italic),
+                          ),
+                          TextSpan(
+                            text: ' Sound fun? \n',
+                          ),
+                          TextSpan(
+                            text: 'Let’s do this!',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: ' 😊🎸✨',
+                          ),
                         ],
                       ),
-                    ),
+                    )
+
                   ),
                 ),
               ],
