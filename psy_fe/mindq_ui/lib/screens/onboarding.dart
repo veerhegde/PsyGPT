90a1931cce83ab97b00c2457175cec8fbe6fd5b5
Veer Hegde <veer165@gmail.com>
2024-12-31 13:42:28 +0530

    added 9 questions

--- psy_fe/mindq_ui/lib/screens/onboarding.dart
+++ psy_fe/mindq_ui/lib/screens/onboarding.dart
@@ -10,47 +10,47 @@ class OnboardingScreen extends StatelessWidget {
         child: buildPersonalityTestWidget(
           questions: [
             Question(
-              text: "Okay, so... do you like hanging out with people more, or are you like, 'Nah, me time, please'?.",
+              text: "I prefer spending time with others over being alone..",
               options: ["Nope!", "Eh.", "Meh.", "Yep!", "Totally!"],
               lottieAnimationUrl: "https://lottie.host/8ef36c39-7b7c-43fb-bff9-f5864f5a10d8/OxcSr390OG.json",
             ),
             Question(
-              text: "When you're looking at something, are you more into all the tiny little details? Like, 'Ooh, look at this tiny thingymajig!' Or do you just go for the big picture?",
+              text: "I focus more on the tiny details than the big picture.",
               options: ["Nope!", "Eh.", "Meh.", "Yep!", "Totally!"],
               lottieAnimationUrl: "https://lottie.host/e487425f-5ce4-4133-a356-901b4d1cdc78/lOiylY7Icn.json",
             ),
             Question(
-              text: "When you're making decisions, are you like, ‘Let’s logic this out, Spock-style,’ or is it more like, ‘What feels right in my heart... or stomach?",
+              text: "I rely on logic more than feelings when making decisions.",
               options: ["Nope!", "Eh.", "Meh.", "Yep!", "Totally!"],
               lottieAnimationUrl: "https://lottie.host/d1f4a79c-0204-4a92-9f5f-d3d906ccdb28/e3QvqgNPR7.json",
             ),
             Question(
-              text: "How do you feel about schedules? Like, are you the kind of person who’s got color-coded planners? Or are you more like, 'Whatever happens, happens'?",
+              text: "I feel comfortable with plans and schedules instead of going with the flow.",
               options: ["Nope!", "Eh.", "Meh.", "Yep!", "Totally!"],
               lottieAnimationUrl: "https://lottie.host/e830bc87-a72b-43df-85bc-3f6e373b4de0/PRUkGNSFsT.json",
             ),
             Question(
-              text: "Ooh! Do you get excited about learning new things? Like, ‘I’m gonna learn how to make a birdhouse today!’ Or is that not your vibe?",
+              text: "Learning new skills and trying new things excites me.",
               options: ["Nope!", "Eh.", "Meh.", "Yep!", "Totally!"],
               lottieAnimationUrl: "https://lottie.host/d1f4a79c-0204-4a92-9f5f-d3d906ccdb28/e3QvqgNPR7.json",
             ),
             Question(
-              text: "Do you ever get those moments where you're like, ‘I just know this is gonna happen,’ and then it does? Like psychic vibes? Oh, I love those!",
+              text: "I often have a strong sense of intuition about what’s going to happen.",
               options: ["Nope!", "Eh.", "Meh.", "Yep!", "Totally!"],
               lottieAnimationUrl: "https://lottie.host/d1f4a79c-0204-4a92-9f5f-d3d906ccdb28/e3QvqgNPR7.json",
             ),
             Question(
-              text: "How important is getting things done quickly and perfectly to you? Like, are you all about efficiency, or do you stop and smell the roses?",
+              text: "Being efficient and productive is very important to me.",
               options: ["Nope!", "Eh.", "Meh.", "Yep!", "Totally!"],
               lottieAnimationUrl: "https://lottie.host/d1f4a79c-0204-4a92-9f5f-d3d906ccdb28/e3QvqgNPR7.json",
             ),
             Question(
-              text: "If someone’s feeling all sad or mopey, are you like, ‘Aww, tell me everything,’ or do you hand them a tissue and hope for the best?",
+              text: "I empathize deeply with other people’s emotions.",
               options: ["Nope!", "Eh.", "Meh.", "Yep!", "Totally!"],
               lottieAnimationUrl: "https://lottie.host/d1f4a79c-0204-4a92-9f5f-d3d906ccdb28/e3QvqgNPR7.json",
             ),
             Question(
-              text: "Okay, last one! Are you the kind of person who loves to just wing it and see what happens? Like, improvise! Woohoo! Or does that freak you out?",
+              text: "Okay, last one! I enjoy being spontaneous and improvising rather than sticking to a plan.",
               options: ["Nope!", "Eh.", "Meh.", "Yep!", "Totally!"],
               lottieAnimationUrl: "https://lottie.host/d1f4a79c-0204-4a92-9f5f-d3d906ccdb28/e3QvqgNPR7.json",
             ),
