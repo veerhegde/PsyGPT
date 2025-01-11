6927692ac1093696442b4b0ba046f7c962551da9
Veer Hegde <veer165@gmail.com>
2025-01-11 16:48:05 +0530

    Merge remote-tracking branch 'origin/veer_dev' into veer_dev
    # Conflicts:
    #	psy_fe/mindq_ui/lib/screens/start_onboard.dart

--- psy_fe/mindq_ui/lib/screens/userauth.dart
+++ psy_fe/mindq_ui/lib/screens/userauth.dart
@@ -27,7 +27,7 @@ class _AuthScreenState extends State<AuthScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFf5f5f5),
+      backgroundColor: const Color(0xffb74093),
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.all(20.0),
@@ -37,7 +37,7 @@ class _AuthScreenState extends State<AuthScreen>
               width: 4,
             ),
             borderRadius: BorderRadius.circular(16),
-            color: Colors.white,
+            color: Color(0xfff4d738),
             boxShadow: const [
               BoxShadow(
                 color: Colors.black,
@@ -51,7 +51,7 @@ class _AuthScreenState extends State<AuthScreen>
               // Tabs
               Container(
                 decoration: BoxDecoration(
-                  color: const Color(0xFFe0e0e0),
+                  color: const Color(0xfff4d738),
                   borderRadius: BorderRadius.circular(12),
                   border: Border.all(color: Colors.black, width: 3),
                 ),
