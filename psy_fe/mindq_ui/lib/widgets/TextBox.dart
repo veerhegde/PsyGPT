66765eebef81fe36f777ea71060033021d2995e2
gouthamarcot <gauthamarcot@yahoo.com>
2025-01-26 12:56:00 +0530

    [ui] added CustomTextBox

--- psy_fe/mindq_ui/lib/widgets/TextBox.dart
+++ psy_fe/mindq_ui/lib/widgets/TextBox.dart
@@ -4,55 +4,79 @@ class CustomTextbox extends StatelessWidget {
   final Color fillColor;
   final String labelText;
   final Color borderColor;
-  final Color iconColor;
   final bool obscureText;
   final IconData? prefixIcon;
   final IconData? suffixIcon;
   final BoxConstraints? constraints;
   final Color textColor;
+  final Color iconColor;
+  final AlignmentGeometry gradientBegin;
+  final AlignmentGeometry gradientEnd;
+  final List<Color> gradientColors;
+  final List<double>? gradientStops;
 
   const CustomTextbox({
     Key? key,
-    this.fillColor = Colors.white,
     required this.labelText,
     this.borderColor = Colors.black,
-    this.iconColor = Colors.grey,
     this.obscureText = false,
+    this.fillColor = Colors.black,
     this.prefixIcon,
     this.suffixIcon,
     this.constraints,
-    this.textColor = Colors.grey,
+    this.textColor = Colors.black,
+    this.iconColor = Colors.grey,
+    this.gradientBegin = Alignment.topLeft,
+    this.gradientEnd = Alignment.bottomRight,
+    this.gradientColors = const [
+      Colors.black,
+      Colors.black,
+      Color(0xfffde900),
+    ],
+    this.gradientStops,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
       constraints: constraints ?? BoxConstraints(),
-      child: TextField(
-        style: TextStyle(color: textColor),
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: fillColor,
-          labelText: labelText,
-          prefixIcon: prefixIcon != null
-              ? Icon(prefixIcon, color: iconColor)
-              : null,
-          suffixIcon: suffixIcon != null
-              ? Icon(suffixIcon, color: iconColor)
-              : null,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(15),
+      child: Container(
+        decoration: BoxDecoration(
+          color: fillColor,
+          gradient: LinearGradient(
+            begin: gradientBegin,
+            end: gradientEnd,
+            colors: gradientColors,
+            stops: gradientStops,
           ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: borderColor, width: 2.0),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: borderColor, width: 1.0),
-            borderRadius: BorderRadius.circular(15),
-          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        obscureText: obscureText,
+        child: TextField(
+          style: TextStyle(color: textColor),
+          decoration: InputDecoration(
+            labelText: labelText,
+            fillColor: fillColor,
+            prefixIcon: prefixIcon != null
+                ? Icon(prefixIcon, color: iconColor)
+                : null,
+            suffixIcon: suffixIcon != null
+                ? Icon(suffixIcon, color: iconColor)
+                : null,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: borderColor, width: 2.0),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: borderColor, width: 1.0),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+          obscureText: obscureText,
+        ),
       ),
     );
   }
