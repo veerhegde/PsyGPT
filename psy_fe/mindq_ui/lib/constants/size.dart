df90df7d46186c229b4e01a15d1d0fd9ab5864bf
gouthamarcot <gauthamarcot@yahoo.com>
2025-01-26 13:04:12 +0530

    [ui] size sheet

---
+++ psy_fe/mindq_ui/lib/constants/size.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const kEmptyWidget = SizedBox.shrink();
+const kGap0 = SizedBox.square(dimension: 0,);
+const kGap5 = SizedBox.square(dimension: 5,);
+const kGap8 = SizedBox.square(dimension: 8,);
+const kPadd2 = EdgeInsets.all(2);
+const kPadd6 = EdgeInsets.all(6);
+const kPadd10 = EdgeInsets.all(10);
+const kPadd15 = EdgeInsets.all(15);
+const kPadd0 = EdgeInsets.zero;
+const kPadd20 = EdgeInsets.all(20);
+const kPadd30 = EdgeInsets.all(30);
+const kPadd40 = EdgeInsets.all(40);
+const kPadd25 = EdgeInsets.all(25);
+const kPadd45 = EdgeInsets.all(45);
+const kPadd50 = EdgeInsets.all(50);
+const kPaddH25 = EdgeInsets.symmetric(horizontal: 25);
+const kPaddH6 = EdgeInsets.symmetric(horizontal: 6);
+const kPaddV10 = EdgeInsets.symmetric(vertical: 10);
+
+final kRadius0 = BorderRadius.circular(0);
+final kRadius10 = BorderRadius.circular(10);
+final kRadius15 = BorderRadius.circular(15);
+final kRadius30 = BorderRadius.circular(30);
+
+const kSizeH45 = Size(0, 45);
+const kSizeH50 = Size(0, 50);
+const kSizeH60 = Size(0, 60);
