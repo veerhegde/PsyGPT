568112cb7e7de7b27e5b487ceb4009e4e17cbd1c
gouthamarcot <gauthamarcot@yahoo.com>
2025-02-02 07:09:53 +0530

    code refractoring

--- psy_fe/mindq_ui/lib/widgets/personality_test_widget.dart
+++ psy_fe/mindq_ui/lib/widgets/personality_test_widget.dart
@@ -131,7 +131,8 @@ class _PersonalityTestContentState extends State<PersonalityTestContent>
                                     dotLottie.animations.values.first,
                                     controller: _lottieControllers[index],
                                     onLoaded: (composition) {
-                                      _lottieControllers[index]!.duration = composition.duration;
+                                      _lottieControllers[index]!.duration =
+                                          composition.duration;
                                     },
                                   );
                                 } else {
@@ -150,15 +151,18 @@ class _PersonalityTestContentState extends State<PersonalityTestContent>
                               controller: _optionPageControllers[index],
                               itemCount: widget.questions[index].options.length,
                               onPageChanged: (optionIndex) {
-                                _updateAnswer(index, (optionIndex + 1).toDouble());
+                                _updateAnswer(
+                                    index, (optionIndex + 1).toDouble());
                               },
                               itemBuilder: (context, optionIndex) {
-                                bool isSelected = (_answers[index] ?? -1) == (optionIndex + 1);
+                                bool isSelected = (_answers[index] ?? -1) ==
+                                    (optionIndex + 1);
                                 return Transform.scale(
                                   scale: isSelected ? 1.1 : 0.6,
                                   child: Container(
                                     height: 120,
-                                    margin: const EdgeInsets.symmetric(vertical: 40),
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 40),
                                     decoration: BoxDecoration(
                                       gradient: const LinearGradient(
                                         colors: [
@@ -169,22 +173,24 @@ class _PersonalityTestContentState extends State<PersonalityTestContent>
                                       ),
                                       borderRadius: BorderRadius.circular(44),
                                       border: isSelected
-                                          ? Border.all(color: Colors.white60, width: 9.6)
+                                          ? Border.all(
+                                              color: Colors.white60, width: 9.6)
                                           : null,
                                       boxShadow: isSelected
                                           ? const [
-                                        BoxShadow(
-                                          color: Colors.white60,
-                                          offset: Offset(0, 4),
-                                          blurRadius: 10,
-                                          spreadRadius: 2,
-                                        ),
-                                      ]
+                                              BoxShadow(
+                                                color: Colors.white60,
+                                                offset: Offset(0, 4),
+                                                blurRadius: 10,
+                                                spreadRadius: 2,
+                                              ),
+                                            ]
                                           : null,
                                     ),
                                     alignment: Alignment.center,
                                     child: Text(
-                                      widget.questions[index].options[optionIndex],
+                                      widget.questions[index]
+                                          .options[optionIndex],
                                       style: ThemeFont.primaryFont(
                                         fontSize: 18,
                                         color: Colors.white,
@@ -198,105 +204,112 @@ class _PersonalityTestContentState extends State<PersonalityTestContent>
                           ),
                         ],
                       );
-                    },),),
-          Container(
-            padding: EdgeInsets.all(20),
-            color: Colors.white.withOpacity(0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                if (_currentPage > 0)
-                  ElevatedButton(
-                    onPressed: () {
-                      _pageController.previousPage(
-                        duration: Duration(milliseconds: 300),
-                        curve: Curves.easeIn,
-                      );
                     },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.grey[300],
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 10,
-                      ),
-                      textStyle: ThemeFont.primaryFont(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    child: Text(
-                      "Previous",
-                      style: ThemeFont.primaryFont(color: Colors.black),
-                    ),
-                  ),
-
-                if (_currentPage == 0)
-                  SizedBox(width: 98),
-
-                Text(
-                  "Q${_currentPage + 1} / ${widget.questions.length}",
-                  style: ThemeFont.primaryFont(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white60,
                   ),
                 ),
                 Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color(0xFF9C3FE4),
-                        Color(0xFFC65647),
-                      ],
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.3), // Shadow color with opacity
-                        offset: Offset(4, 4), // Horizontal and vertical shadow offset
-                        blurRadius: 10, // How blurry the shadow is
-                        spreadRadius: 2, // How far the shadow spreads
+                  padding: EdgeInsets.all(20),
+                  color: Colors.white.withOpacity(0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      if (_currentPage > 0)
+                        ElevatedButton(
+                          onPressed: () {
+                            _pageController.previousPage(
+                              duration: Duration(milliseconds: 300),
+                              curve: Curves.easeIn,
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey[300],
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 10,
+                            ),
+                            textStyle: ThemeFont.primaryFont(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: Text(
+                            "Previous",
+                            style: ThemeFont.primaryFont(color: Colors.black),
+                          ),
+                        ),
+                      if (_currentPage == 0) SizedBox(width: 98),
+                      Text(
+                        "Q${_currentPage + 1} / ${widget.questions.length}",
+                        style: ThemeFont.primaryFont(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white60,
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFF9C3FE4),
+                              Color(0xFFC65647),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(
+                                  0.3), // Shadow color with opacity
+                              offset: Offset(4,
+                                  4), // Horizontal and vertical shadow offset
+                              blurRadius: 10, // How blurry the shadow is
+                              spreadRadius: 2, // How far the shadow spreads
+                            ),
+                          ],
+                        ),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_currentPage < widget.questions.length - 1) {
+                              _pageController.nextPage(
+                                duration: Duration(milliseconds: 300),
+                                curve: Curves.easeIn,
+                              );
+                            } else {
+                              widget.onTestComplete();
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors
+                                .transparent, // Make button background transparent
+                            shadowColor:
+                                Colors.transparent, // Remove default shadow
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                          child: Text(
+                            _currentPage < widget.questions.length - 1
+                                ? "Next"
+                                : "Finish",
+                            style: ThemeFont.primaryFont(
+                                fontSize: 17.92,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_currentPage < widget.questions.length - 1) {
-                        _pageController.nextPage(
-                          duration: Duration(milliseconds: 300),
-                          curve: Curves.easeIn,
-                        );
-                      } else {
-                        widget.onTestComplete();
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent, // Make button background transparent
-                      shadowColor: Colors.transparent, // Remove default shadow
-                      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-    child: Text(
-    _currentPage < widget.questions.length - 1
-    ? "Next"
-        : "Finish",
-    style: ThemeFont.primaryFont(fontSize: 17.92,
-        fontWeight: FontWeight.bold, color: Colors.white),
-    ),
-                    ),
-                  ),
+                ),
               ],
             ),
           ),
         ],
       ),
-    ),
-    ],),
     );
   }
 
